044c9e5145afe9e96718e0a469b28347fea97aa6
nikhil240896 <myselfnikhilkr@gmail.com>
2025-02-26 13:41:11 +0530

    Update Next.js

--- Next.js
+++ Next.js
@@ -1,1 +1,8 @@
+pm2 start npm --name "Plannit-Frontend" -- start
+pm2 restart 1
+pm2 logs 1
+pm2 delete Plannit-Frontend
+pm2 list
+pm2 start npm --name "Plannit-Frontend" -- start -- --hostname 0.0.0.0  // for Listing of frontend (Next.js)
+pm2 status
 
